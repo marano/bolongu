5bd4e82322abbf44d40457e07bf2e1f6ef813f58
Marano <thiagomarano@gmail.com>
2009-03-19 13:50:44 -0300

    added sharethis

---
+++ app/helpers/share_this_helper.rb
@@ -0,0 +1,13 @@
+module ShareThisHelper
+
+  def share_this_javascript_include
+    javascript_include_tag "http://w.sharethis.com/button/sharethis.js#tabs=web%2Cpost%2Cemail&amp;charset=utf-8&amp;style=default&amp;publisher=81fcfee2-503d-4740-9625-84fea115aec6"
+  end
+
+  def share_this(title, url)
+    "<script type=\"text/javascript\">
+      SHARETHIS.addEntry({ title: \"#{title}\", url: \"#{url}\" });
+    </script>"
+  end
+
+end
