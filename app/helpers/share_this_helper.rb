eaae421fba0856564c1bc850c4206e525f7e1fcb
Marano <thiagomarano@gmail.com>
2009-03-19 22:50:32 -0300

    fixed color for share it

--- app/helpers/share_this_helper.rb
+++ app/helpers/share_this_helper.rb
@@ -1,7 +1,7 @@
 module ShareThisHelper
 
   def share_this_javascript_include
-    javascript_include_tag "http://w.sharethis.com/button/sharethis.js#tabs=web%2Cpost%2Cemail&amp;charset=utf-8&amp;style=default&amp;publisher=81fcfee2-503d-4740-9625-84fea115aec6"
+    javascript_include_tag "http://w.sharethis.com/button/sharethis.js#tabs=web%2Cpost%2Cemail&amp;charset=utf-8&amp;style=default&amp;publisher=81fcfee2-503d-4740-9625-84fea115aec6&amp;headerbg=%234c8542"
   end
 
   def share_this(title, url)
