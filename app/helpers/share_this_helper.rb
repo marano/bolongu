e1d36bdd418311ae9e0a220a6a40bcb5952bdd8a
Marano <thiagomarano@gmail.com>
2009-04-01 12:27:58 -0300

    configured new sharethis button

--- app/helpers/share_this_helper.rb
+++ app/helpers/share_this_helper.rb
@@ -1,7 +1,7 @@
 module ShareThisHelper
 
   def share_this_javascript_include
-    javascript_include_tag "http://w.sharethis.com/button/sharethis.js#tabs=web%2Cpost%2Cemail&amp;charset=utf-8&amp;style=default&amp;publisher=81fcfee2-503d-4740-9625-84fea115aec6&amp;headerbg=%234c8542"
+    javascript_include_tag "http://w.sharethis.com/button/sharethis.js#publisher=81fcfee2-503d-4740-9625-84fea115aec6&amp;type=website&amp;buttonText=share&amp;send_services=email%2Csms%2Caim%2Cmyspace&amp;post_services=orkut%2Cfacebook%2Ctwitter%2Cblogger%2Cwordpress%2Cdigg%2Cgoogle_bmarks%2Cwindows_live%2Cdelicious%2Clinkedin%2Cybuzz%2Cstumbleupon%2Creddit%2Ctechnorati%2Cmixx%2Ctypepad%2Cmyspace%2Cfark%2Cbus_exchange%2Cpropeller%2Cnewsvine&amp;headerbg=%234D7B41&amp;linkfg=%23707070&amp;headerTitle=Bolongu!"
   end
 
   def share_this(title, url)
