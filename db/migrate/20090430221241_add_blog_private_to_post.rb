d8904e12d250cf7a3cd6415ab7e1301b0da3dc42
Marano <thiagomarano@gmail.com>
2009-04-30 20:02:37 -0300

    fixed migration

--- db/migrate/20090430221241_add_blog_private_to_post.rb
+++ db/migrate/20090430221241_add_blog_private_to_post.rb
@@ -1,6 +1,7 @@
 class AddBlogPrivateToPost < ActiveRecord::Migration
   def self.up
     add_column :posts, :blog_private, :boolean, :default => false
+    Post.update_all(:blog_private => false)
   end
 
   def self.down
