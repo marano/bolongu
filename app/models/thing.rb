a4bf262fa28284109f35cae0b6c1eacf9978b804
Marano <thiagomarano@gmail.com>
2009-05-04 01:01:42 -0300

    fixed things layout and validations to thing

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -7,8 +7,12 @@ class Thing < ActiveRecord::Base
   has_attached_file :photo, :styles => { :default => ["320x240>", :jpg], :small => ["100x100>", :jpg], :tiny => ["50x50>", :jpg] }
   has_attached_file :attachment
 
+  validates_presence_of :email
+
   validates_attachment_content_type :photo, :content_type => [ 'image/jpeg', 'image/png', 'image/bmp' , 'image/gif' ]
   validates_attachment_size :photo, :less_than => 5.megabytes
+  
+  validates_attachment_size :attachment, :less_than => 20.megabytes
 
   default_scope :order => 'created_at DESC'
 
