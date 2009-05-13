9c4a29056cce8b2c4fee0bc2c28f12f595b72d1e
Marano <thiagomarano@gmail.com>
2009-05-12 22:35:54 -0300

    configured for displaying nil errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+config.whiny_nils = true
+
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
