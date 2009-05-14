54d646a72f89822f4b9e0ad26d4cec99392ec3b7
Marano <thiagomarano@gmail.com>
2009-05-14 13:18:19 -0300

    disabling buttons

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => 'Account'
 
   default_scope :order => 'created_at DESC'
+  
+  validates_presence_of :body
 
   alias :account :author
 
